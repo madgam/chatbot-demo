95578ff56904b092936f8e7a4d4fc966f01c11bb
武藤祐治 <madgam0731@gmail.com>
2020-05-07 13:59:10 +0900

    delete unnecessary rows

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-// import '../assets/styles/styles.css';
-
-const useStyles = makeStyles((theme) => ({
-  root: {},
-}));
 
 const Answer = (props) => {
-  // const classes = useStyles();
-
   return (
     <Button
       variant='contained'
