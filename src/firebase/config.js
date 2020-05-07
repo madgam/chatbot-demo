6914ccb0f1d837331308beefcefaba91d8f3ea3b
武藤祐治 <madgam0731@gmail.com>
2020-05-07 17:46:32 +0900

    add firebase functions

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyBW29Z5KYBbQbK1vWWMJcXM3Cn4cw3qMi8',
+  apiKey: process.env.REACT_APP_GOOGLE_FIREBASE_API_KEY,
   authDomain: 'chatbot-demo-ec566.firebaseapp.com',
   databaseURL: 'https://chatbot-demo-ec566.firebaseio.com',
   projectId: 'chatbot-demo-ec566',
