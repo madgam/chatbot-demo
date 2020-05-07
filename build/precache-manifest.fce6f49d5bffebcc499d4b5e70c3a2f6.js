b3cca5b9eebbf1a2a73162b5c15797b4588de44d
武藤祐治 <madgam0731@gmail.com>
2020-05-07 10:23:51 +0900

    modified class components

---
+++ build/precache-manifest.fce6f49d5bffebcc499d4b5e70c3a2f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676204e2dd3ce4fc6e8f9fa66224db6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64503d51ef6e0d3df433",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a48a2078fbd6f3b071a1",
+    "url": "/static/js/2.fc672179.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc672179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64503d51ef6e0d3df433",
+    "url": "/static/js/main.5a2fedf1.chunk.js"
+  },
+  {
+    "revision": "4229509eeb462a01809d",
+    "url": "/static/js/runtime-main.6f4cdfdb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
